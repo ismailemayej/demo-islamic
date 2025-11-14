7f91f5a13ad6ec1929c8e6ea9bee6105b57b5948
ismailemayej <salikbd.com@gmail.com>
2025-11-15 01:33:09 +0600

    dashboard updated

--- app/(common)/layout.tsx
+++ app/(common)/layout.tsx
@@ -2,7 +2,6 @@ import { Navbar } from "@/components/navbar/Navbar";
 import { Container } from "@/components/container";
 import TopAdBanner from "@/components/advesment";
 import { Footer } from "@/components/footer/Footer";
-import { Providers } from "@/components/providers";
 
 export default function HomeLayout({
   children,
@@ -13,9 +12,7 @@ export default function HomeLayout({
     <div className="relative flex flex-col h-screen">
       <TopAdBanner />
       <Navbar />
-      <Container>
-        <Providers>{children}</Providers>
-      </Container>
+      <Container>{children}</Container>
       <Footer />
     </div>
   );
