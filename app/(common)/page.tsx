d0df881e7756e27078db6213271f49f1887fdcaf
ismailemayej <salikbd.com@gmail.com>
2025-11-27 22:05:07 +0600

    jodit editor added

--- app/(common)/page.tsx
+++ app/(common)/page.tsx
@@ -3,11 +3,13 @@ import { WebsiteData } from "@/types/allData";
 
 export default async function Home() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/all-data`, {
-    cache: "no-store",
+    next: { revalidate: 5 },
   });
+
   if (!res.ok) {
     throw new Error("Failed to fetch website data");
   }
+
   const data: WebsiteData[] = await res.json();
   return <Main data={data} />;
 }
